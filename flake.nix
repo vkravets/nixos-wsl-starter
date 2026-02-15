175ea346cf47d939ee02f34c78e844bd862d68a2
Vladimir Kravets <vova.kravets@gmail.com>
2026-02-15 12:29:39 +0000

    Update to 25.11 and adopt to my needs

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   description = "NixOS configuration";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  inputs.home-manager.url = "github:nix-community/home-manager/release-24.11";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-25.11";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.nur.url = "github:nix-community/NUR";
--- wsl.nix
+++ wsl.nix
@@ -8,7 +8,7 @@
   ...
 }: {
   # FIXME: change to your tz! look it up with "timedatectl list-timezones"
-  time.timeZone = "America/Los_Angeles";
+  time.timeZone = "Europe/Kyiv";
 
   networking.hostName = "${hostname}";
 
@@ -31,7 +31,7 @@
     extraGroups = [
       "wheel"
       # FIXME: uncomment the next line if you want to run docker without sudo
-      # "docker"
+      "docker"
     ];
     # FIXME: add your own hashed password
     # hashedPassword = "";
@@ -58,7 +58,7 @@
     startMenuLaunchers = true;
 
     # Enable integration with Docker Desktop (needs to be installed)
-    docker-desktop.enable = false;
+    docker-desktop.enable = true;
   };
 
   virtualisation.docker = {
@@ -70,20 +70,20 @@
   # FIXME: uncomment the next block to make vscode running in Windows "just work" with NixOS on WSL
   # solution adapted from: https://github.com/K900/vscode-remote-workaround
   # more information: https://github.com/nix-community/NixOS-WSL/issues/238 and https://github.com/nix-community/NixOS-WSL/issues/294
-  # systemd.user = {
-  #   paths.vscode-remote-workaround = {
-  #     wantedBy = ["default.target"];
-  #     pathConfig.PathChanged = "%h/.vscode-server/bin";
-  #   };
-  #   services.vscode-remote-workaround.script = ''
-  #     for i in ~/.vscode-server/bin/*; do
-  #       if [ -e $i/node ]; then
-  #         echo "Fixing vscode-server in $i..."
-  #         ln -sf ${pkgs.nodejs_18}/bin/node $i/node
-  #       fi
-  #     done
-  #   '';
-  # };
+  systemd.user = {
+    paths.vscode-remote-workaround = {
+      wantedBy = ["default.target"];
+      pathConfig.PathChanged = "%h/.vscode-server/bin";
+   };
+    services.vscode-remote-workaround.script = ''
+      for i in ~/.vscode-server/bin/*; do
+        if [ -e $i/node ]; then
+          echo "Fixing vscode-server in $i..."
+         ln -sf ${pkgs.nodejs}/bin/node $i/node
+        fi
+      done
+    '';
+  };
 
   nix = {
     settings = {
